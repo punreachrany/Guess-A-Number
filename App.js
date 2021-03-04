e69927c062c4135b8f8e9c45bec0e8ef2aa15cef
punreachrany <punreach.rany@yahoo.com>
2021-03-04 13:32:15 +0900

    Minor edition

--- App.js
+++ App.js
@@ -1,15 +1,28 @@
+import React, { useState } from "react";
 import { StyleSheet, Text, View } from "react-native";
 
+import GameScreen from "./screens/GameScreen";
 import Header from "./components/Header";
-import React from "react";
 import StartGameScreen from "./screens/StartGameScreen";
 import { StatusBar } from "expo-status-bar";
 
 export default function App() {
+  const [userNumber, setUserName] = useState();
+
+  const startGameHandler = (selectedNumber) => {
+    setUserName(selectedNumber);
+  };
+
+  let content = <StartGameScreen onStartGame={startGameHandler} />;
+
+  if (userNumber) {
+    content = <GameScreen userChoice={userNumber} />;
+  }
+
   return (
     <View style={styles.screen}>
       <Header title="Guess a number" />
-      <StartGameScreen />
+      {content}
     </View>
   );
 }
--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -1,7 +1,8 @@
-import { useState } from "react";
-import { StyleSheet, Text, View } from "react-native";
-
+import { Button, StyleSheet, Text, View } from "react-native";
 import React, { useState } from "react";
+
+import Card from "../components/Card";
+import NumberContainer from "../components/NumberContainer";
 
 const generateRandomBetween = (min, max, exclude) => {
   min = Math.ceil(min);
@@ -18,7 +19,31 @@ const GameScreen = (props) => {
   const [currentGuess, setCurrentGuess] = useState(
     generateRandomBetween(1, 100, props.userChoice)
   );
+
+  return (
+    <View style={styles.screen}>
+      <Text>Opponent's Guess</Text>
+      <NumberContainer>{currentGuess}</NumberContainer>
+      <Card style={styles.buttonContainer}>
+        <Button title="LOWER" onPress={() => {}} />
+        <Button title="GREATER" onPress={() => {}} />
+      </Card>
+    </View>
+  );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  screen: {
+    flex: 1,
+    padding: 10,
+    alignItems: "center",
+  },
+  buttonContainer: {
+    flexDirection: "row",
+    justifyContent: "space-around",
+    marginTop: 20,
+    width: 300,
+    maxWidth: "80%",
+  },
+});
 export default GameScreen;
